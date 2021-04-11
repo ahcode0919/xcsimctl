fb4e4f2ac08815ba030e6ef1118776c77e0b0de3
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2021-04-11 11:53:07 -0700

    Add Vapor Template (#1)

---
+++ Sources/App/Shell/Shell.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aaron Hinton on 4/11/21.
+//
+
+import Foundation
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
